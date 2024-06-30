0020b25c2afea117e23e3fae044168074dc57ab6
Gonzalo Gorosito <gonzalo.gorosito@medable.com>
2024-06-30 19:26:22 +0200

    feat(server): Adding API url for geoJson output.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import cors from 'cors'
 const app = express();
 const PORT = 3000;
-const APP_URL = 'https://data.sfgov.org/resource/rqzj-sfat.json'
+const APP_URL_RAW = 'https://data.sfgov.org/resource/rqzj-sfat.json'
+const APP_URL_GEO = 'https://data.sfgov.org/resource/rqzj-sfat.geojson'
 const APP_TOKEN = "F3ChZibCo6q64PmptV4pkxfwE"
 
 // Enabling CORS
@@ -11,12 +12,13 @@ app.use(cors());
 
 app.get('/data', async (req, res) => {
     try {
-        const response = await axios.get(APP_URL, {
+        const response = await axios.get(APP_URL_GEO, {
             params: {
-                $limit: 5000,
+                $limit: 50,
                 $$app_token: APP_TOKEN // TODO : Set ENV later.
             }
         });
+        console.log("SERVER", { data: response.data })
         res.json(response.data);
     } catch (error) {
         res.status(500).send(error.toString());
