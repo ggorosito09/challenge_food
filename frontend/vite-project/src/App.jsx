b5dc2695a6e2cbb92a103f37cf7b107751034d5e
Gonzalo Gorosito <gonzalo.gorosito@medable.com>
2024-06-30 18:46:28 +0200

    feat(app): Adding first mock version of FE talking to BE

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -16,18 +30,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h3>Challange GMG = Vite + React</h3>
+      <div className="App">
+        <h4>Data from Backend</h4>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
