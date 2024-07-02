566f7ecda4783b6986bb9032f03a1a70cae5069d
Gonzalo Gorosito <gonzalo.gorosito@medable.com>
2024-07-02 16:25:39 +0200

    feat(server): Adding post filter of items by some conditions
    After looking into more detail, the data is polluted and not well to send all of them.
    
    TODO: Could be improved by leveraring the SoQL

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import cors from 'cors'
 const app = express();
 const PORT = 3000;
+// TODO: It would be good to have a way to shift between the outputs.
 const APP_URL_RAW = 'https://data.sfgov.org/resource/rqzj-sfat.json'
 const APP_URL_GEO = 'https://data.sfgov.org/resource/rqzj-sfat.geojson'
 const APP_TOKEN = "F3ChZibCo6q64PmptV4pkxfwE"
@@ -10,15 +11,52 @@ const APP_TOKEN = "F3ChZibCo6q64PmptV4pkxfwE"
 // Enabling CORS
 app.use(cors());
 
+// TODO: This should get separated in a particular router, then we can add middlewares for authentication, error, logging, etc.
 app.get('/data', async (req, res) => {
     try {
+
+        const {
+            $limit,
+            $offset,
+            // By the time, this is the only paramter we receive from FE
+            $q
+        } = req.query
+
         const response = await axios.get(APP_URL_GEO, {
             params: {
-                $limit: 50,
+                $limit,
+                $offset,
+                $q,
                 $$app_token: APP_TOKEN // TODO : Set ENV later.
             }
-        });
-        console.log("SERVER", { data: response.data })
+        })
+
+        /**
+         * The API returns a lot of data
+         * - With/without coordinates.
+         * - Some of them without even "fooditems"
+         * - Many of them are not "APPROVED"
+         * 
+         * So here it is hardcoded a post processing of the items.
+         * We could manipulate this through the API SoQL
+         */
+        response.data.features = response.data.features.filter(
+            (truckFeature) => {
+                return truckFeature.properties &&
+                    truckFeature.properties.applicant &&
+                    truckFeature.properties.fooditems &&
+                    truckFeature.properties.latitude &&
+                    truckFeature.properties.latitude !== '0' &&
+                    truckFeature.properties.longitude &&
+                    truckFeature.properties.longitude !== '0' &&
+                    // approved recently
+                    truckFeature.properties.status &&
+                    truckFeature.properties.status === 'APPROVED' &&
+                    truckFeature.properties.approved &&
+                    new Date(truckFeature.properties.approved).getFullYear() >= 2020
+            }
+
+        );
         res.json(response.data);
     } catch (error) {
         res.status(500).send(error.toString());
