dca73d2560a31bdddc0ec0298aec491414c792d0
Gonzalo Gorosito <gonzalo.gorosito@medable.com>
2024-07-02 16:19:40 +0200

    fix(UI fetchData): Fixing querystring parameter for $q

--- frontend/vite-project/src/utils/fetchData.js
+++ frontend/vite-project/src/utils/fetchData.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // src/utils/fetchData.js
 export const fetchData = async (query) => {
     try {
-        const response = await axios.get(`http://localhost:3000/data?q=${query}`);
+        const response = await axios.get(`http://localhost:3000/data?$q=${query}`);
         return response.data.features;
     } catch (error) {
         console.error('Error fetching data:', error);
