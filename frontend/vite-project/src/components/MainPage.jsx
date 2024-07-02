e62afe3137c623ee533f7fd417bd03bf2df211d6
Gonzalo Gorosito <gonzalo.gorosito@medable.com>
2024-07-02 16:06:39 +0200

    refactor(MainPage): Removed button, style refactor and cleaned

--- frontend/vite-project/src/components/MainPage.jsx
+++ frontend/vite-project/src/components/MainPage.jsx
@@ -29,10 +29,6 @@ function MainPage() {
         setQuery(e.target.value);
     };
 
-    const handleSearchSubmit = (e) => {
-        e.preventDefault();
-        fetchData(query).then(data => setTrucks(data));
-    };
 
     const handleRowClick = (truck) => {
         setSelectedTruck(truck);
@@ -42,30 +38,27 @@ function MainPage() {
         <div className="main-container">
             <div className="content-container">
                 <div className="input-table-container">
-                <div className="header-container">
-                <img src={logo} alt="Logo" className="logo-main" />
-                <h5 className="title">Food Truck Finder</h5>
-            </div>
+                    <div className="header-container">
+                        <img src={logo} alt="Logo" className="logo-main" />
+                        <h5 className="title">Food Truck Finder</h5>
+                    </div>
                     <div className="input-container">
-                        <form onSubmit={handleSearchSubmit}>
-                            <input
-                                type="text"
-                                placeholder="I would like... Fried Chicken"
-                                value={query}
-                                onChange={handleSearchChange}
-                            />
-                            <button type="submit">Search</button>
-                        </form>
+                        <input
+                            type="text"
+                            placeholder="I would like... Fried Chicken"
+                            value={query}
+                            onChange={handleSearchChange}
+                        />
                     </div>
                     <div className="table-container">
                         <TruckTable trucks={trucks} onRowClick={handleRowClick} />
                     </div>
                 </div>
-                {trucks.length > 0 && (
-                    <div className="map-container">
-                        <MapView trucks={trucks} selectedTruck={selectedTruck} />
-                    </div>
-                )}
+
+                <div className="map-container">
+                    <MapView trucks={trucks} selectedTruck={selectedTruck} />
+                </div>
+
             </div>
         </div>
     );
